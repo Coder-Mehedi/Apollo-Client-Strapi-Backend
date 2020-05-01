cf70f816b692ad992a06df1a1c85d29879cefe40
unknown <mehedi5051@gmail.com>
2020-05-01 21:52:47 +0600

    seprate category list on its own component

--- src/components/category/Categories.js
+++ src/components/category/Categories.js
@@ -2,22 +2,23 @@ import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_CATEGORIES } from "../../graphql";
+import CategoryList from "./CategoryList";
 
 const Categories = () => {
 	const { loading, data } = useQuery(GET_CATEGORIES);
-	let categoryList;
-	if (data) {
-		console.log(data);
-		categoryList = data.categories.map((cat) => (
-			<li key={cat.id}>{cat.name}</li>
-		));
-	} else {
-		categoryList = <h5>Loading Categories...</h5>;
-	}
+
 	return (
 		<div className="center">
 			<h2>Category List</h2>
-			<ul>{categoryList}</ul>
+			<ul>
+				{data ? (
+					data.categories.map((category) => (
+						<CategoryList key={category.id} category={category} />
+					))
+				) : (
+					<h5>Loading Categories...</h5>
+				)}
+			</ul>
 		</div>
 	);
 };
